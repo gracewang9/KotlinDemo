b64e6fb48d0d20df2d0929617f0662e7e847c16a
gracewang9 <896922176@qq.com>
2024-06-22 10:21:49 +0800

    initial commit

---
+++ Kotlin.kt
@@ -0,0 +1,14 @@
+class Kotlin {
+
+    fun sol() {
+        /**
+         * 声明变量
+         */
+        val num = 0
+        var str = "string"
+
+        //空安全
+        val dub:String?
+        val d:String?=null
+    }
+}
